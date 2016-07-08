476b10ed5b34b3670053f49064b7506369abe868
michael anjia <michael anjia>
2016-07-08 15:49:34 +0300

    division working with ajax

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -44,8 +44,21 @@
         var sn=parseFloat(document.getElementById('sn').value);
         var rs=document.getElementById('result');
             rs.innerHTML=fn/sn;
+         var results='&num1='+ fn + '&num2='+ sn +'&result='+ result;
+
+// AJAX code to submit form.
+        
+$.ajax({
+type: "POST",
+url: "http://192.168.1.130:7000/compute",
+data:results,
+cache: false,
+success: function(html) {
+   
+           alert("Division Performed");   
+}
+});
     });
-    
-    }
+ }
  document.addEventListener("app.Ready", register_event_handlers, false);
 })();
